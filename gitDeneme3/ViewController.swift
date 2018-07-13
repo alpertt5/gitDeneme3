2b50089691d7f30207500eee60c3583642efa71c
Alper <tellioglualper@gmail.com>
2018-07-13 23:11:21 +0300

    branch 5 is ready for action

--- gitDeneme3/ViewController.swift
+++ gitDeneme3/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("branch!!!")
+        
+        print("branch5")
     }
 
     override func didReceiveMemoryWarning() {
